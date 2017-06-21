1490b6f40e3fee8bb156352d99854338fc00c58c
RomanDG <romand85@yandex.ru>
2017-06-21 17:26:26 +0300

    Исправил 2 задание, учел все ошибки

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,8 +1,14 @@
 
 let records = [
   {
-    img:  'http://placehold.it/200x100',
+    id: 1,
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum esse eveniet quae incidunt labore eaque delectus, nisi, ad, voluptatem veritatis dicta ipsum molestias iusto commodi nobis vitae deleniti perspiciatis cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quos, sit vel voluptates, velit similique explicabo error laboriosam architecto fugiat odio iusto temporibus possimus cupiditate. Excepturi officiis, assumenda dolorum natus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla tenetur, repudiandae nam fuga, suscipit magnam a est consectetur recusandae? Ratione fuga quae at sapiente magni laudantium optio dolores dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem provident earum sunt aliquam iusto laboriosam voluptatum tempora magni, commodi consequuntur, reprehenderit laborum, doloribus possimus enim amet. Ab, dolorem doloremque consequatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quibusdam nobis quam quia, ab quaerat harum accusamus qui quis nostrum nesciunt, saepe dignissimos. Perspiciatis eum ut incidunt ipsam dolore magnam!',
+    img: {
+      alt: "about image",
+      src: "http://placehold.it/200x100",
+      width: "200px",
+      height: "100px"
+    },
     metaData: {
       author: "Elizabeth Humphrey",
       createDate: "суббота, 16 июля 2017 15:30",
@@ -11,8 +17,14 @@ let records = [
     }
   },
   {
-    img:  'http://placehold.it/200x100',
+    id: 2,
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum esse eveniet quae incidunt labore eaque delectus, nisi, ad, voluptatem veritatis dicta ipsum molestias iusto commodi nobis vitae deleniti perspiciatis cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quos, sit vel voluptates, velit similique explicabo error laboriosam architecto fugiat odio iusto temporibus possimus cupiditate. Excepturi officiis, assumenda dolorum natus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla tenetur, repudiandae nam fuga, suscipit magnam a est consectetur recusandae? Ratione fuga quae at sapiente magni laudantium optio dolores dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem provident earum sunt aliquam iusto laboriosam voluptatum tempora magni, commodi consequuntur, reprehenderit laborum, doloribus possimus enim amet. Ab, dolorem doloremque consequatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quibusdam nobis quam quia, ab quaerat harum accusamus qui quis nostrum nesciunt, saepe dignissimos. Perspiciatis eum ut incidunt ipsam dolore magnam!',
+    img: {
+      alt: "about image",
+      src: "http://placehold.it/200x100",
+      width: "200px",
+      height: "100px"
+    },    
     metaData: {
       author: "Robbins Carney",
       createDate: "суббота, 16 июля 2017 11:30",
@@ -21,8 +33,14 @@ let records = [
     }
   },
   {
-    img:  'http://placehold.it/200x100',
+    id: 3,
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum esse eveniet quae incidunt labore eaque delectus, nisi, ad, voluptatem veritatis dicta ipsum molestias iusto commodi nobis vitae deleniti perspiciatis cum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quos, sit vel voluptates, velit similique explicabo error laboriosam architecto fugiat odio iusto temporibus possimus cupiditate. Excepturi officiis, assumenda dolorum natus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nulla tenetur, repudiandae nam fuga, suscipit magnam a est consectetur recusandae? Ratione fuga quae at sapiente magni laudantium optio dolores dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem provident earum sunt aliquam iusto laboriosam voluptatum tempora magni, commodi consequuntur, reprehenderit laborum, doloribus possimus enim amet. Ab, dolorem doloremque consequatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quibusdam nobis quam quia, ab quaerat harum accusamus qui quis nostrum nesciunt, saepe dignissimos. Perspiciatis eum ut incidunt ipsam dolore magnam!',
+    img: {
+      alt: "about image",
+      src: "http://placehold.it/200x100",
+      width: "200px",
+      height: "100px"
+    },    
     metaData: {
       author: "Rose Lawson",
       createDate: "суббота, 16 июля 2017 14:50",
@@ -34,14 +52,14 @@ let records = [
 
 class Image extends React.Component {
   render(){
-      const {img} = this.props;
+      const {alt, src, width, height} = this.props.img;
       return (
         <img 
-          src={img} 
-          alt='about image'
+          src={src} 
+          alt={alt}
           style={{
-            width: "200px",
-            height: "100px",
+            width: {width},
+            height: {height},
             float: "left",
             margin: "5px 5px 5px 0"
           }}
@@ -75,7 +93,7 @@ class LikeBtn extends React.Component {
   }
 
   handleClick(e){
-    this.setState({currentlike: ++this.state.currentLike})
+    this.setState({currentLike: this.state.currentLike+1})
   }
 
   render(){
@@ -110,7 +128,7 @@ class BlogList extends React.Component {
     const {records} = this.props;
     return (     
       <div>
-        { records.map((item)=>{return <BlogItem {...item} />; })}
+        { records.map((item)=>(<BlogItem {...item} key={item.id} />))}
       </div>
     )
   }
