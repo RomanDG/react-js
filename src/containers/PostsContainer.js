63b8eef213ea33f202fffd0978df81f8a0f1b24d
RomanDG <romand85@yandex.ru>
2017-08-03 11:31:32 +0300

    исправленое

--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -2,13 +2,42 @@ import {connect} from 'react-redux';
 import Posts from 'components/widgets/blog/Posts';
 import {fetchPosts} from 'actions/Posts';
 import { Item } from 'semantic-ui-react';
-import {getPosts} from 'components/helpers/GetPosts'
 
+const allPosts = (posts, pathName, searchString) => {
+  if(pathName == '/' && searchString == null){
+    return posts.map((post) => post )
+  }
+  return false;
+} // получение всех постов
+
+const onePost = (posts, pathName, searchString) => {
+  if(pathName != '/' && searchString == null){
+    return posts.map((post) => {
+        if (post.id == Number((pathName).replace(/\D+/g,''))) {
+          return post;
+        }
+    })
+  }
+  return false;
+} // получение поста по url
+
+const foundPosts = (posts, searchString) => {
+  if(searchString != null){
+    return posts.map((post) => {
+      if (post.title.toLowerCase().includes(searchString.toLowerCase()) == true) {
+        return post;
+      }
+    })
+  }
+  return false;
+} // получение постов по поиску
 
 const mapStateToProps = (state) => ({
-  posts:  getPosts(state),
+  posts:  allPosts(state.posts.posts, state.router.location.pathname, state.posts.searchStr) || 
+          onePost(state.posts.posts, state.router.location.pathname, state.posts.searchStr) || 
+          foundPosts(state.posts.posts, state.posts.searchStr),
   router: state.router.location.pathname
-});
+}); 
 
 const mapDispatchToProps = (dispatch) => ({
   fetchPosts: () => dispatch(fetchPosts())
