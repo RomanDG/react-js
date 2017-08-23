b5f4e3927b95a32916a03d162b731c5dff61d7fd
RomanDG <romand85@yandex.ru>
2017-08-23 20:31:48 +0300

    добавил тесты на рендеринг и отображение компонента

---
+++ src/components/widgets/blog/__tests__/post.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {render} from 'react-dom';
+import TextBox from 'components/widgets/blog/elements/TextBox';
+
+import Post from 'components/widgets/blog/Post';
+
+describe("Post", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<TextBox />, div);
+  })
+})
+
+describe("render", () => {
+  it("should render the text", () => {
+    const textProps = { text: 'Text for testing the TextBox' };
+    const textbox = shallow(<TextBox {...textProps} />);
+    const text = <span>Text for testing the TextBox</span>;
+    expect(textbox.contains(text)).toEqual(true);
+  })
+})
